7051045ffcaaf9f55d3e78b753e8a54e66f494cb
Maryam Mahmoud <m.mahmoud@travelshoppe.co.ke>
2016-10-31 15:01:49 +0300

    FizzBuzz

---
+++ Fizz_Buzz.py
@@ -0,0 +1,11 @@
+def fizz_buzz(x):
+# The FizzBuzz algorithm applied to value x
+    if x % 3 == 0 and x % 5 == 0:
+        return "FizzBuzz"
+    elif x % 3 == 0:
+        return "Fizz"
+    elif x % 5 == 0:
+        return "Buzz"
+    else:
+        return (x)
+print (x)
