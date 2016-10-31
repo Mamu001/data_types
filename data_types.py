29b45cd05101f16ab66176ba9b890c2fd8cee958
Maryam Mahmoud <m.mahmoud@travelshoppe.co.ke>
2016-10-31 12:58:17 +0300

    data_types_programming_logic

---
+++ data_types.py
@@ -0,0 +1,20 @@
+def data_type(x):
+	if type(x) == None:
+		return 'no value'
+	if type(x) == str:
+		return len(x)
+	elif type(x) == bool:
+		return x
+	elif type(x) == int:
+		if x < 100:
+			return "less than 100"
+		elif x == 100:
+			return 'equal to 100'
+		else:
+			return "more than 100"
+	elif type(x) == list:
+		if len(x) < 3:
+			return None
+		else:
+			return x[2]
+# print data_type(True)
